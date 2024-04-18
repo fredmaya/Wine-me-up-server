14236bebfc233e44fd501a49364eae88ae347ac6
fredmaya <fredericomaya@icloud.com>
2024-04-17 19:56:00 -0700

    dessert wines

---
+++ migrations/20240412214336_dessertwines_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  return knex.schema.createTable("dessertwines", function (table) {
+    table.increments("id").primary();
+    table.string("image");
+    table.string("winery");
+    table.string("wine");
+    table.string("price_range");
+    table.text("description");
+    table.string("food_pairing");
+    table.string("appetizer_pairing");
+    table.string("dessert_pairing");
+    table.string("country");
+  });
+}
+
+export async function down(knex) {
+  return knex.schema.dropTable("dessertwines");
+}
